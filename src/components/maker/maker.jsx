f8bcc647844d76b263327d6da7b0d0d864c405a0
dngwoo <akfmzk84@naver.com>
2020-12-05 20:31:44 +0900

    add card and card_editor_form

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
@@ -7,6 +7,41 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "dngwoo",
+      company: "Recursivesoft",
+      theme: "dark",
+      title: "Software Enginner",
+      email: "dngwoo@gamil.com",
+      message: "go for it",
+      fileName: "dngwoo",
+      fileURL: "dngwoo.png",
+    },
+    {
+      id: "2",
+      name: "dngwoo",
+      company: "Recursivesoft",
+      theme: "dark",
+      title: "Software Enginner",
+      email: "dngwoo@gamil.com",
+      message: "go for it",
+      fileName: "dngwoo",
+      fileURL: "dngwoo.png",
+    },
+    {
+      id: "3",
+      name: "dngwoo",
+      company: "Recursivesoft",
+      theme: "dark",
+      title: "Software Enginner",
+      email: "dngwoo@gamil.com",
+      message: "go for it",
+      fileName: "dngwoo",
+      fileURL: "dngwoo.png",
+    },
+  ]);
   const history = useHistory();
 
   const onLogout = () => {
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout}></Header>
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer></Footer>
     </section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Card from "../card/card";
 import styles from "./preview.module.css";
 
-const Preview = (props) => (
+const Preview = ({ cards }) => (
   <section className={styles.preview}>
     <h1 className={styles.title}>Card review</h1>
+    {cards.map((card) => (
+      <Card card={card} key={card.id} />
+    ))}
   </section>
 );
 
